f6256351a5f16f492930c97973d67ec21fa6a0ac
Drew <drew.r.wilson@gmail.com>
2014-08-22 21:53:30 -0700

    better readme & some scraping stuff

--- README.md
+++ README.md
@@ -1,13 +1,57 @@
 scrape-vegan-outreach-data
 ==========================
 
-Scrapes data about leafleting numbers from Vegan Outreach's AdoptACollege webpages
+This app collects data about leafleting numbers from Vegan Outreach's [AdoptACollege webpages](http://adoptacollege.org) and saves them as comma-seperated value files (CSV files).
 
-This script hits various endpoints on adoptacollege.org, scrape the data, and then saves the data as CSVs
+It hits various endpoints on [adoptacollege.org](http://adoptacollege.org), scrapes the data, and then writes the data to a file.
 
-How to use:
+How to use locally:
 * Fork or clone this code
 * Run ``npm install`` in your code directory to install dependencies
 * Run ``node app.js``
 
 Data will be saved in ``data/`` in CSV format
+
+
+= Data =
+
+== Schools ==
+[ ] Semester totals
+[ ] Daily totals
+[ ] Leafleters by lifetime
+[ ] Leafleters by Semester
+[ ] Biggest events
+[ ] Individual schools
+[ ] Individual states
+
+== Venues ==
+[ ] Year totals
+[ ] Daily totals
+[ ] Leafleters by lifetime
+[ ] Leafleters by year
+[ ] Biggest events
+[ ] Warped tour
+[ ] All events
+
+== Combined ==
+[ ] Grand totals
+[ ] Daily totals
+[ ] Leafleters by lifetime
+[ ] Summer totals
+
+
+
+= Ideas =
+== Ideas for possible improvements ==
+* Make an API for this data
+  * But build an API directly from the actual  DB as opposed to using scraping to build an API
+* Make some tests to make sure the data coming out is accurate and formatted properly.
+ * Maybe a CSV linter
+* Schedule regular scraping
+* Automatically upload CSVs to a public place after scraping
+
+
+== Hair-brained ideas ==
+* Scrape daily and tweet when there's a new update
+* Tweet a random featured leafleter and their stats
+* Associate twitter handles with names in the data and tweet @mentioning someone when there's a change to their data
--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var request = require('request');
 var fs = require("fs");
 var cheerio = require("cheerio");
 
-url = 'http://www.adoptacollege.org/stats/daily.php';
-
 function ensureExists(path, mask, cb) {
     if (typeof mask == 'function') { // allow the `mask` parameter to be optional
         cb = mask;
@@ -43,23 +41,77 @@ function writeToCSV(array, filename, callback) {
   });
 }
 
-function cleanUpArray (array, filename) {
-  array.forEach(function(element,index,array){
-     if (index > 0 ) {
-       array[index][0] = array[index][0].killWhiteSpaceAndPeriod();
-       array[index][1] = '"' + array[index][1] + '"';
-       array[index][2] = array[index][2].killComma();
-     }
-  })
+function cleanUpArray (array, filename, typeOfCleanUp) {
+  if (typeOfCleanUp == 'dailyTotals') {
+    array.forEach(function(element,index,array){
+       if (index > 0 ) {
+         array[index][0] = array[index][0].killWhiteSpaceAndPeriod();
+         array[index][1] = '"' + array[index][1] + '"';
+         array[index][2] = array[index][2].killComma();
+       }
+    })
+  } else if (typeOfCleanUp == 'semesterTotals') {
+      //TODO: remove the 5th column: array[4]
+      array.forEach(function(element,index,array){
+         if (index > 0 ) {
+           array[index][3] = array[index][3].killComma();
+           //remove 5th column here
+         }
+      })
+  }
 
   writeToCSV(array, filename, function () {
     console.log('Successfully wrote data to CSV: ' + filename);
   })
 }
 
-function scrapeDailyTotals(dailyTotals, currentPage, lastPage, filename) {
+function scrapeSemesterTotals (semesterTotals, url, filename) {
+  console.log('Scraping semester totals now...');
+  formParams = {
+   "var1" : "",
+   "var2" : "",
+   "var3" : "",
+   "var4" : "",
+   "var5" : "",
+   "var6" : ""
+  }
 
-  console.log('Scraping daily totals currentPage: ' + currentPage + '...');
+request({
+  uri: url,
+  method: "POST",
+  timeout: 10000,
+  followRedirect: true,
+  form: formParams,
+  maxRedirects: 10
+}, function(error, response, body) {
+
+  //load body into cheerio
+  var $ = cheerio.load(body);
+
+
+  //loop through all rows <tr>
+  $('table tr').map(function(i, row) {
+      var item = [];
+      //loop through all columns <td>
+      $(row).children('td').each(function() {
+        item.push( $(this).text() );
+      });
+
+      if (item.length > 0) {
+        semesterTotals.push(item);
+      }
+    })
+  cleanUpArray(semesterTotals, filename, 'semesterTotals');
+
+  });
+
+
+} //scrapeSemesterTotals
+
+
+function scrapeDailyTotals(dailyTotals, url, currentPage, lastPage, filename) {
+
+  console.log('Scraping semester totals currentPage: ' + currentPage + ' of ' + lastPage+ '...');
   formParams = {
    "var1" : currentPage,
    "var2" : "campus",
@@ -96,7 +148,7 @@ function scrapeDailyTotals(dailyTotals, currentPage, lastPage, filename) {
 
       })
       if (currentPage >= lastPage)  {
-        cleanUpArray(dailyTotals, filename);
+        cleanUpArray(dailyTotals, filename, 'dailyTotals');
       } else {
         scrapeDailyTotals(dailyTotals, currentPage+1, lastPage, filename);
       }
@@ -120,15 +172,22 @@ function initialize (directory, callback) {
 
 function main () {
   console.log('Initializing...');
-
   var config = {
                 "outputDirectory" : 'data/',
                 "dailyTotals" : {
                       "headers" : ['Rank','Date','Amount'],
                       "firstPage" : 1,
                       "lastPage" : 2,
-                      "filename" : 'daily-totals.csv'
-                    }
+                      "filename" : 'daily-totals.csv',
+                      "url" : 'http://www.adoptacollege.org/stats/daily.php'
+                    },
+                "semesterTotals" : {
+                      "headers" : ['Semester','Schools','Leafleters', 'Booklets'],
+                      "firstPage" : 1,
+                      "lastPage" : 1,
+                      "filename" : 'semester-totals.csv',
+                      "url" : 'http://www.adoptacollege.org/stats/semesters_table.php'
+                    },
                 }
   console.log('Loaded config.');
 
@@ -136,16 +195,18 @@ function main () {
     console.log('Using this directory for data output: ' + config.outputDirectory);
 
     //scrape daily totals and save to a CSV
-    scrapeDailyTotals([config.dailyTotals.headers], config.dailyTotals.firstPage, config.dailyTotals.lastPage, config.outputDirectory + config.dailyTotals.filename);
+
+    //schools data:
+    //scrapeDailyTotals([config.dailyTotals.headers], config.dailyTotals.url, config.dailyTotals.firstPage, config.dailyTotals.lastPage, config.outputDirectory + config.dailyTotals.filename);
+    scrapeSemesterTotals([config.semesterTotals.headers], config.semesterTotals.url, config.outputDirectory + config.semesterTotals.filename);
+
+
+
+    //venues data:
+
+    //grand totals data:
 
   })
-
-
-
-
-
-
-
 }
 
 main();
